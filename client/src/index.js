539ec9a9803fa1deee6c101be997b86c9c6ff03c
Virginia <WIlliam-Champion@outlook.com>
2023-04-12 03:21:13 -0300

    qwas134

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import {Provider} from 'react-redux';
 
 import { BrowserRouter } from 'react-router-dom';
 import store from './Store/store';
+import axios from "axios";
 //axios.defaults.baseURL = "http://localhost:3001/" ;
 axios.defaults.baseURL = "https://app-videogames-production.up.railway.app/" ;
 ReactDOM.render(
