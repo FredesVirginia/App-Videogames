0d2aed4cdf2ae6d37743a2c4635d34e463cce4fb
Virginia <WIlliam-Champion@outlook.com>
2023-04-12 06:40:45 -0300

    qw34

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 import store from './Store/store';
 import axios from "axios";
 //axios.defaults.baseURL = "http://localhost:3001/" ;
-axios.defaults.baseURL = "https://app-videogames-production.up.railway.app/" ;
+axios.defaults.baseURL = "app-videogames-production.up.railway.app" ;
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
