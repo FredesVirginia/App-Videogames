0adbf5e692c8e354e5dd4a3c0252fad9e05c1070
Virginia <WIlliam-Champion@outlook.com>
2023-04-11 22:56:19 -0300

    d

--- api/index.js
+++ api/index.js
@@ -18,11 +18,12 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
+require('dotenv').config();
 const { conn } = require('./src/db.js');
-
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
